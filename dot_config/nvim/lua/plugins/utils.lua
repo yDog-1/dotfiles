358c62e844fa0fdd571587a9c8ceb8e1dc3102fa
yDog <ydog-1@outlook.jp>
2025-02-16 21:02:41 +0900

    add treemonkey.nvim

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -182,4 +182,24 @@ return {
 		},
 		opts = {},
 	},
+	-- 範囲選択を補助
+	{
+		"atusy/treemonkey.nvim",
+		keys = {
+			{
+				"m",
+				function()
+					require("treemonkey").select({ ignore_injections = false })
+				end,
+				mode = { "x", "o" },
+				desc = "Select block",
+			},
+		},
+		lazy = true,
+		config = function()
+			require("denops-lazy").load("treemonkey")
+		end,
+	},
+	-- which-key
+	require("plugins/which-key"),
 }
