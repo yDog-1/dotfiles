d050ccee64f0fafe6c530fe3257e2a7e4c6d988a
yDog <ydog-1@outlook.jp>
2025-02-16 21:04:31 +0900

    feat denops-lazy.nvim denops-shared-server.vim

--- dot_config/nvim/lua/plugins/denops.vim.lua
+++ dot_config/nvim/lua/plugins/denops.vim.lua
@@ -1,12 +1,25 @@
 return {
-  "vim-denops/denops.vim",
-  lazy = true,
-  init = function()
-    vim.g["denops#server#deno_args"] = {
-      "-q",
-      "--no-lock",
-      "-A",
-      "--unstable-kv",
-    }
-    end,
+	{
+		"vim-denops/denops.vim",
+		lazy = false,
+		dependencies = {
+			"vim-denops/denops-shared-server.vim",
+		},
+		init = function()
+			vim.g["denops#server#deno_args"] = {
+				"-q",
+				"--no-lock",
+				"-A",
+				"--unstable-kv",
+			}
+			vim.g["denops_server_addr"] = "127.0.0.1:32123"
+		end,
+		build = function()
+			vim.fn["denops_shared_server#install"]()
+		end,
+	},
+	{
+		"yuki-yano/denops-lazy.nvim",
+		opts = {},
+	},
 }
