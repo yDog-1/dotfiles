4e1961108d0e54b589ba20e1cdf16b0c33d53157
yDog <ydog-1@outlook.jp>
2025-02-16 21:10:36 +0900

    fix neo-tree.nvim

--- dot_config/nvim/lua/plugins/neo-tree.nvim.lua
+++ dot_config/nvim/lua/plugins/neo-tree.nvim.lua
@@ -1,23 +1,21 @@
 return {
-  {
-    -- ファイルブラウザ
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    keys = {
-      -- 設定ディレクトリ `~/.config/nvim`を開く
-      { "<Leader>.", ":Neotree filesystem left dir=~/.config/nvim<CR>" },
-      -- ネオツリーを開く
-      { "<Leader>e", ":Neotree filesystem reveal left<CR>" },
-    },
-    cmd = "Neotree",
-    opts = {
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_gitignored = false,
-          hide_hidden = false,
-        },
-      },
-    },
-  },
+	{
+		-- ファイルブラウザ
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		keys = {
+			-- ネオツリーを開く
+			{ "<Leader>e", ":Neotree filesystem reveal left<CR>", desc = "Open neotree" },
+		},
+		cmd = "Neotree",
+		opts = {
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_hidden = false,
+				},
+			},
+		},
+	},
 }
