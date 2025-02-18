81f93f17f918c297237c3f3a200cc76ed574d38c
yDog <ydog-1@outlook.jp>
2025-02-18 20:04:00 +0900

    add gin keys autocmd

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -40,6 +40,31 @@ return {
 		},
 		config = function()
 			require("denops-lazy").load("vim-gin", { wait_load = false })
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "gin-diff", "gin-log", "gin-status" },
+				callback = function()
+					local keymap = vim.keymap.set
+					local opts = { buffer = true, noremap = true }
+					keymap({ "n" }, "c", "<Cmd>Gin commit<Cr>", opts)
+					keymap({ "n" }, "s", "<Cmd>GinStatus<Cr>", opts)
+					keymap({ "n" }, "L", "<Cmd>GinLog --graph --oneline<Cr>", opts)
+					keymap({ "n" }, "d", "<Cmd>GinDiff --cached<Cr>", opts)
+					keymap({ "n" }, "q", "<Cmd>bdelete<Cr>", opts)
+					keymap({ "n" }, "p", [[<Cmd>lua vim.notify("Gin push")<Cr><Cmd>Gin push<Cr>]], opts)
+					keymap({ "n" }, "P", [[<Cmd>lua vim.notify("Gin pull")<Cr><Cmd>Gin pull<Cr>]], opts)
+				end,
+			})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "gin-status",
+				callback = function()
+					local keymap = vim.keymap.set
+					local opts = { buffer = true, noremap = true }
+					keymap({ "n" }, "h", "<Plug>(gin-action-stage)", opts)
+					keymap({ "n" }, "l", "<Plug>(gin-action-unstage)", opts)
+				end,
+			})
 		end,
 	},
 	{
