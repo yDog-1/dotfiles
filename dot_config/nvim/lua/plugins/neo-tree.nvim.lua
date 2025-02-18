5085ef6766aed1a24565d4b84a6d5c4ff0732862
yDog <ydog-1@outlook.jp>
2025-02-18 10:32:46 +0900

    feat autocmd rename on move file

--- dot_config/nvim/lua/plugins/neo-tree.nvim.lua
+++ dot_config/nvim/lua/plugins/neo-tree.nvim.lua
@@ -8,14 +8,26 @@ return {
 			{ "<Leader>e", ":Neotree filesystem reveal left<CR>", desc = "Open neotree" },
 		},
 		cmd = "Neotree",
-		opts = {
-			filesystem = {
+		opts = function(_, opts)
+			local events = require("neo-tree.events")
+			opts.event_handlers = opts.event_handlers or {}
+
+			-- ファイルの移動時に呼び出し元のリネーム処理を行う
+			local function on_move(data)
+				---@diagnostic disable-next-line: undefined-global
+				require("snacks.rename").on_rename_file(data.source, data.destination)
+			end
+			vim.list_extend(opts.event_handlers, {
+				{ event = events.FILE_MOVED, handler = on_move },
+				{ event = events.FILE_RENAMED, handler = on_move },
+			})
+
+			opts.filesystem = {
 				filtered_items = {
 					hide_dotfiles = false,
 					hide_gitignored = false,
 					hide_hidden = false,
 				},
-			},
-		},
+		end,
 	},
 }
