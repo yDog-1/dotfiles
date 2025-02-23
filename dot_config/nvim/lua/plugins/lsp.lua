6ae5deae67eac01522e4dc817f0c0484068d2e49
yDog <ydog-1@outlook.jp>
2025-02-23 20:43:47 +0900

    change: formatter to efm

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,19 +1,39 @@
-local lsp_servers = {
-	"lua_ls",
-	"ts_ls",
-	"gopls",
-}
-
 require("plugins.which-key.spec").add({
 	mode = "n",
 	{ "<Leader>c", group = "code" },
 })
+
+local format = function()
+	vim.lsp.buf.format({ name = "efm" })
+end
+
+local ensure_installed = {
+	-- LS
+	"efm",
+	"lua_ls",
+	"ts_ls",
+	"gopls",
+	"sqls",
+	"graphql",
+	"jsonls",
+	"yamlls",
+
+	-- Linter
+	"biome",
+	"commitlint",
+	"golangci-lint",
+	"hadolint",
+
+	-- Formatter
+	"stylua",
+}
 
 return {
 	{
 		"williamboman/mason.nvim",
 		dependencies = {
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			"creativenull/efmls-configs-nvim",
 		},
 		lazy = true,
 		opts = true,
@@ -30,6 +50,38 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local nvim_lsp = require("lspconfig")
+
+			local stylua = require("efmls-configs.formatters.stylua")
+			local languages = {
+				lua = { stylua },
+			}
+
+			local efmls_config = {
+				filetypes = vim.tbl_keys(languages),
+				settings = {
+					rootMarkers = { ".git/" },
+					languages = languages,
+				},
+				init_options = {
+					documentFormatting = true,
+					documentRangeFormatting = true,
+				},
+			}
+
+			-- 保存時にフォーマット
+			local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
+			vim.api.nvim_create_autocmd("BufWritePost", {
+				group = lsp_fmt_group,
+				callback = function(ev)
+					local efm = vim.lsp.get_clients({ name = "efm", bufnr = ev.buf })
+
+					if vim.tbl_isempty(efm) then
+						return
+					end
+
+					format()
+				end,
+			})
 
 			require("mason-lspconfig").setup_handlers({
 				function(server_name)
@@ -54,6 +106,12 @@ return {
 						},
 					})
 				end,
+
+				efm = function()
+					require("lspconfig").efm.setup(vim.tbl_extend("force", efmls_config, {
+						cabilities = capabilities,
+					}))
+				end,
 			})
 		end,
 	},
@@ -64,8 +122,24 @@ return {
 			"williamboman/mason.nvim",
 		},
 		lazy = true,
+	},
+	-- efm Language Server と nvim-lspconfig の連携
+	{
+		"creativenull/efmls-configs-nvim",
+		version = "v1.x.x",
+		lazy = true,
+		keys = {
+			{ "<Leader>cf", format, desc = "format current file" },
+		},
+		dependencies = { "neovim/nvim-lspconfig" },
+		opts = {},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		lazy = true,
 		opts = {
-			ensure_installed = lsp_servers,
+			auto_update = true,
+			ensure_installed = ensure_installed,
 		},
 	},
 	-- UI/UXを改善
