783c2080d6597c9eb5cac4eeb2cd890734cf7c4b
yDog <ydog-1@outlook.jp>
2025-03-01 18:03:44 +0900

    add fidget.nvim
    the plugin for lsp progress notification

--- dot_config/nvim/lua/plugins/lsp/init.lua
+++ dot_config/nvim/lua/plugins/lsp/init.lua
@@ -162,4 +162,16 @@ return {
 			},
 		},
 	},
+	-- LSP progress notification
+	{
+		"j-hui/fidget.nvim",
+		opts = {
+			notification = {
+				window = {
+					-- 背景を透過
+					winblend = 0,
+				},
+			},
+		},
+	},
 }
