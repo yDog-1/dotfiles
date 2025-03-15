7d32a855d9066bd4899de8d9a8135f2b48a066c2
yDog <ydog-1@outlook.jp>
2025-03-15 17:38:05 +0900

    feat(neovim): add in-and-out.nvim plugin
    、ysmb-wtsg/in-and-out.nvim プラグインで、挿入モードで Ctrl+Enter のキーバインディングを設定

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -228,4 +228,16 @@ return {
 			{ "<Leader>Q", "<cmd>Qfreplace<CR>", desc = "Replace in quickfix" },
 		},
 	},
+	{
+		"ysmb-wtsg/in-and-out.nvim",
+		keys = {
+			{
+				"<M-m>",
+				function()
+					require("in-and-out").in_and_out()
+				end,
+				mode = "i",
+			},
+		},
+	},
 }
