0582a9b91b55d78ccdd3c74b17a7293a453d3eb9
yDog <ydog-1@outlook.jp>
2025-03-19 21:45:21 +0900

    feat(neovim): add nvim-treesitter-textobjects plugin

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,10 @@ return {
 	{
 		-- 何もしなくても vim-polyglot といい感じに連携してくれる
 		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			-- treesitter を利用した textobject を追加する
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
 		event = { "BufReadPre", "BufNewFile" },
 		build = ":TSUpdate",
 		config = function()
@@ -12,6 +16,55 @@ return {
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },
+				textobjects = {
+					swap = {
+						enable = true,
+						swap_next = {
+							["g>"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["g<"] = "@parameter.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]m"] = "@function.outer",
+							["]]"] = { query = "@class.outer", desc = "Next class start" },
+
+							-- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
+							["]o"] = "@loop.*",
+							-- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+
+							-- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+							-- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+							["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+							["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+						},
+						goto_next_end = {
+							["]M"] = "@function.outer",
+							["]["] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[["] = "@class.outer",
+						},
+						goto_previous_end = {
+							["[M"] = "@function.outer",
+							["[]"] = "@class.outer",
+						},
+						-- Below will go to either the start or the end, whichever is closer.
+						-- Use if you want more granular movements
+						-- Make it even more gradual by adding multiple queries and regex.
+						goto_next = {
+							["]d"] = "@conditional.outer",
+						},
+						goto_previous = {
+							["[d"] = "@conditional.outer",
+						},
+					},
+				},
 			})
 
 			-- 折り畳みの設定
