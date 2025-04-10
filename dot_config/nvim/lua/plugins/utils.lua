a6775b15a984414e6f8bf78980717b4d2cda7abc
yDog <ydog-1@outlook.jp>
2025-04-10 21:59:17 +0900

    feat(neovim): add dial.nvim
    dial.nvimプラグインを導入し、CTRL-AとCTRL-Xによる増減機能を拡張
    - キーマッピングを設定
    - グループ操作のためのg<C-a>とg<C-x>キーマップを追加
    - booleanの切り替え機能も追加

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -216,4 +216,80 @@ return {
 			},
 		},
 	},
+	-- CTRL-A, CTRL-X の強化
+	{
+		"monaqa/dial.nvim",
+		event = "BufReadPost",
+		keys = {
+			{
+				"<C-a>",
+				function()
+					require("dial.map").manipulate("increment", "normal")
+				end,
+				desc = "Increment",
+			},
+			{
+				"<C-x>",
+				function()
+					require("dial.map").manipulate("decrement", "normal")
+				end,
+				desc = "Decrement",
+			},
+			{
+				"g<C-a>",
+				function()
+					require("dial.map").manipulate("increment", "gnormal")
+				end,
+				desc = "Increment",
+			},
+			{
+				"g<C-x>",
+				function()
+					require("dial.map").manipulate("decrement", "gnormal")
+				end,
+				desc = "Decrement",
+			},
+			{
+				"<C-a>",
+				function()
+					require("dial.map").manipulate("increment", "visual")
+				end,
+				mode = "v",
+				desc = "Increment",
+			},
+			{
+				"<C-x>",
+				function()
+					require("dial.map").manipulate("decrement", "visual")
+				end,
+				mode = "v",
+				desc = "Decrement",
+			},
+			{
+				"g<C-a>",
+				function()
+					require("dial.map").manipulate("increment", "gvisual")
+				end,
+				mode = "v",
+				desc = "Increment",
+			},
+			{
+				"g<C-x>",
+				function()
+					require("dial.map").manipulate("decrement", "gvisual")
+				end,
+				mode = "v",
+				desc = "Decrement",
+			},
+		},
+		config = function()
+			local augend = require("dial.augend")
+			local default = require("dial.config").augends:get("default")
+			require("dial.config").augends:register_group({
+				default = vim.list_extend(default, {
+					augend.constant.alias.bool,
+				}),
+			})
+		end,
+	},
 }
