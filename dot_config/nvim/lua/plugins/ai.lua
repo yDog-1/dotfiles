2a50259bbac16074f4b3b73aad62aca3fb42801f
yDog <ydog-1@outlook.jp>
2025-04-14 21:57:53 +0900

    feat(neovim): MCPHub統合の追加
    - MCPHubプラグインをインストールして設定
    - AvanteとCodeCompanion拡張機能の統合
    - MCPHubと競合するツールを無効化
    - MCPツールをCodeCompanionに追加

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -48,6 +48,30 @@ end)()
 
 return {
 	{
+		"ravitemer/mcphub.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
+		},
+		-- comment the following line to ensure hub will be ready at the earliest
+		cmd = "MCPHub", -- lazy load by default
+		build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+		-- uncomment this if you don't want mcp-hub to be available globally or can't use -g
+		-- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+		config = function()
+			require("mcphub").setup({
+				auto_approve = false,
+				extensions = {
+					avante = {},
+					codecompanion = {
+						show_result_in_chat = true, -- Show tool results in chat
+						make_vars = true, -- Create chat variables from resources
+						make_slash_commands = true, -- make /slash_commands from MCP server prompts
+					},
+				},
+			})
+		end,
+	},
+	{
 		"yetone/avante.nvim",
 		event = "VeryLazy",
 		build = avante_build,
@@ -128,6 +152,31 @@ return {
 				provider = "telescope",
 				provider_opts = {},
 			},
+			--
+			-- mcphub関連の設定
+			--
+			-- mcpとコンフリクトするため、無効にする
+			disabled_tools = {
+				"list_files", -- Built-in file operations
+				"search_files",
+				"read_file",
+				"create_file",
+				"rename_file",
+				"delete_file",
+				"create_dir",
+				"rename_dir",
+				"delete_dir",
+				"bash", -- Built-in terminal access
+			},
+			system_prompt = function()
+				local hub = require("mcphub").get_hub_instance()
+				return hub:get_active_servers_prompt()
+			end,
+			custom_tools = function()
+				return {
+					require("mcphub.extensions.avante").mcp_tool(),
+				}
+			end,
 		},
 	},
 	{
@@ -239,6 +288,15 @@ return {
 							return "  CodeCompanion (" .. adapter.formatted_name .. ")"
 						end,
 						user = "  Me",
+					},
+					tools = {
+						["mcp"] = {
+							-- Prevent mcphub from loading before needed
+							callback = function()
+								return require("mcphub.extensions.codecompanion")
+							end,
+							description = "Call tools and resources from the MCP Servers",
+						},
 					},
 				},
 			},
