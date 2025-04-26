36e4df4c565ff0a0d36e2a019b0e6e68113c9a63
yDog <ydog-1@outlook.jp>
2025-04-26 14:32:57 +0900

    feat(nvim): add event for codecompletion
    BufEnterイベントでプラグインをロードするように変更

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -82,7 +82,7 @@ return {
 			"nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
 		},
 		-- comment the following line to ensure hub will be ready at the earliest
-	event = "BufEnter",
+		event = "BufEnter",
 		cmd = "MCPHub", -- lazy load by default
 		build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
 		-- uncomment this if you don't want mcp-hub to be available globally or can't use -g
