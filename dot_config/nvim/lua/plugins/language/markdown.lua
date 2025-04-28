8130eb1d55253c13aa740db7c011831108837f7f
yDog <ydog-1@outlook.jp>
2025-04-28 19:16:44 +0900

    feat(nvim): add otter.nvim plugin
    otter.nvimプラグインを追加して、マークダウンファイル内の埋め込みコードブロックのサポートを強化しました。マークダウンファイルを開いた際に自動的にOtterを有効化するautocommandも設定しています。

--- dot_config/nvim/lua/plugins/language/markdown.lua
+++ dot_config/nvim/lua/plugins/language/markdown.lua
@@ -62,4 +62,22 @@ return {
 			require("markview").setup(opts)
 		end,
 	},
+	{
+		"jmbuhr/otter.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {},
+		config = function(_, opts)
+			local otter = require("otter")
+			otter.setup(opts)
+
+			vim.api.nvim_create_autocmd("filetype", {
+				pattern = "markdown",
+				callback = function()
+					otter.activate(nil, true, false)
+				end,
+			})
+		end,
+	},
 }
