6d8561e506ac8a094070c07eb56fc51eed4e677b
yDog <ydog-1@outlook.jp>
2025-05-17 12:04:56 +0900

    feat(nvim): add schemastore for Neovim LSP
    Adds support for completions on JSON and YAML files using schemastore.

--- dot_config/nvim/lua/plugins/lsp/init.lua
+++ dot_config/nvim/lua/plugins/lsp/init.lua
@@ -64,6 +64,7 @@ return {
 		dependencies = {
 			"williamboman/mason-lspconfig.nvim",
 			"Shougo/ddc-source-lsp",
+			"b0o/schemastore.nvim",
 		},
 		event = { "BufReadPre" },
 		cmd = "Mason",
@@ -103,6 +104,30 @@ return {
 								"missing-fields",
 							},
 						},
+					},
+				},
+			})
+
+			vim.lsp.config("jsonls", {
+				settings = {
+					json = {
+						schemas = require("schemastore").json.schemas(),
+						validate = { enable = true },
+					},
+				},
+			})
+
+			vim.lsp.config("yamlls", {
+				settings = {
+					yaml = {
+						schemaStore = {
+							-- You must disable built-in schemaStore support if you want to use
+							-- this plugin and its advanced options like `ignore`.
+							enable = false,
+							-- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+							url = "",
+						},
+						schemas = require("schemastore").yaml.schemas(),
 					},
 				},
 			})
