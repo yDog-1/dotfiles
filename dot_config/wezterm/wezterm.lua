250656d784e05b44fea33abc4f9cc4a1a56ec54e
yDog <ydog-1@outlook.jp>
2025-08-12 18:04:39 +0900

    feat(wezterm): change font from HWNF to JPDOC
    Moralerspace Argon has been updated to use the JPDOC font instead of HWNF.

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -47,7 +47,7 @@ config.color_schemes = {
 -- フォント設定
 config.font = wezterm.font_with_fallback({
 	{
-		family = "Moralerspace Argon HWNF",
+		family = "Moralerspace Argon JPDOC",
 	},
 	"MonaspiceAr NF",
 })
