742e7e1e88d9b49581f6988da1e461ea39725122
yDog <ydog-1@outlook.jp>
2025-08-20 20:18:11 +0900

    fix(shell): remove aliases for alternative tools
    Remove alternative tool's aliases. Because I already remenber the tool's
    name, so I don't need the alias anymore.

--- dot_config/home-manager/modules/shell/aliases.nix
+++ dot_config/home-manager/modules/shell/aliases.nix
@@ -12,12 +12,6 @@
       "l" = "eza -F";
       "tree" = "eza --tree";
 
-      # catの代替
-      "cat" = "bat";
-
-      # findの代替
-      "find" = "fd";
-
       # 便利コマンド
       "reload" = "source ~/.zshrc";
       "path" = "echo $PATH | tr ':' '\n'";
