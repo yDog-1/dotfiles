03c0897f237f7d264e5883ec2a40fb7b1de03abf
yDog <ydog-1@outlook.jp>
2025-08-20 21:29:16 +0900

    feat(nvim): add doge plugin for code documentation
    the doge plugin provides a way to generate documentation comments in
    various programming languages.

---
+++ dot_config/nvim/lua/plugins/vim-doge.lua
@@ -0,0 +1,11 @@
+return {
+	"kkoomen/vim-doge",
+	lazy = false,
+	init = function()
+		vim.g.doge_enable_mappings = 0
+	end,
+	config = function()
+		vim.cmd("silent call doge#install()")
+    vim.keymap.set("n", "<leader>cm", "<cmd>DogeGenerate<CR>", { desc = "Generate Documentation" })
+	end,
+}
