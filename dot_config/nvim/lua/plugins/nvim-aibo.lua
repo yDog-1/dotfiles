5bd818e28d982f32043d7ac162bef6b994369f67
yDog <ydog-1@outlook.jp>
2025-10-13 19:51:15 +0900

    feat(nvim): add some keybindings for nvim-aibo

--- dot_config/nvim/lua/plugins/nvim-aibo.lua
+++ dot_config/nvim/lua/plugins/nvim-aibo.lua
@@ -15,8 +15,9 @@ return {
 			disable_startinsert_on_insert = false,
 			disable_startinsert_on_startup = true,
 			prompt = {
+				no_default_mappings = true,
 				on_attach = function(bufnr)
-          vim.o.hidden = true
+					vim.o.hidden = true
 					vim.api.nvim_create_autocmd("VimLeavePre", {
 						group = vim.api.nvim_create_augroup("AiboClose", { clear = true }),
 						buffer = bufnr,
@@ -24,19 +25,42 @@ return {
 							vim.cmd("bwipeout! " .. bufnr)
 						end,
 					})
+
+					vim.keymap.set("n", "q", "<cmd>q<cr><cmd>q<cr>", { buffer = bufnr, desc = "Close Aibo" })
+					vim.keymap.set("n", "<C-q>", "<cmd>q<cr><cmd>bw!<cr>", { buffer = bufnr, desc = "Wipeout Aibo" })
+					vim.keymap.set("n", "<C-c>", "<cmd>q<cr><cmd>bw!<cr>", { buffer = bufnr, desc = "Wipeout Aibo" })
+					vim.keymap.set(
+						{ "n", "i" },
+						"<C-i>",
+						"<plug>(aibo-submit)<esc><cmd>wincmd k<cr>",
+						{ buffer = bufnr, desc = "Submit prompt" }
+					)
+					vim.keymap.set("n", "K", "<plug>(aibo-send)<up>", { buffer = bufnr, desc = "Up arrow" })
+					vim.keymap.set("n", "J", "<plug>(aibo-send)<down>", { buffer = bufnr, desc = "Down arrow" })
+					vim.keymap.set("n", "<Enter>", "<plug>(aibo-send)<Enter>", { buffer = bufnr, desc = "Enter" })
+					vim.keymap.set("n", "<Esc>", "<plug>(aibo-send)<Esc>", { buffer = bufnr, desc = "Escape" })
 				end,
 			},
-      console = {
-        on_attach = function(bufnr)
-          vim.api.nvim_create_autocmd("VimLeavePre", {
-            group = vim.api.nvim_create_augroup("AiboClose", { clear = true }),
-            buffer = bufnr,
-            callback = function()
-              vim.cmd("bwipeout! " .. bufnr)
-            end,
-          })
-        end,
-      }
+			console = {
+				no_default_mappings = true,
+				on_attach = function(bufnr)
+					vim.api.nvim_create_autocmd("VimLeavePre", {
+						group = vim.api.nvim_create_augroup("AiboClose", { clear = true }),
+						buffer = bufnr,
+						callback = function()
+							vim.cmd("bwipeout! " .. bufnr)
+						end,
+					})
+
+					vim.keymap.set("n", "q", "<cmd>q<cr>", { buffer = bufnr, desc = "Close Aibo console" })
+					vim.keymap.set("n", "<C-c>", "<cmd>bw!<cr>", { buffer = bufnr, desc = "Wipeout Aibo console" })
+					vim.keymap.set("n", "<C-q>", "<cmd>bw!<cr>", { buffer = bufnr, desc = "Wipeout Aibo console" })
+					vim.keymap.set("n", "K", "<plug>(aibo-send)<up>", { buffer = bufnr, desc = "Up arrow" })
+					vim.keymap.set("n", "J", "<plug>(aibo-send)<down>", { buffer = bufnr, desc = "Down arrow" })
+					vim.keymap.set("n", "<Enter>", "<plug>(aibo-send)<Enter>", { buffer = bufnr, desc = "Enter" })
+					vim.keymap.set("n", "<Esc>", "<plug>(aibo-send)<Esc>", { buffer = bufnr, desc = "Escape" })
+				end,
+			},
 		})
 	end,
 }
