9ffa5aa001b0a82a0d605e5f114cc3b7606bad53
yDog <ydog-1@outlook.jp>
2025-10-13 16:15:13 +0900

    feat(nvim): add config for nvim-aibo plugin
    add some basic configuration for the nvim-aibo plugin that's included:
    - disable startinsert on startup
    - wipeout the buffers on vim close
    - use `resume` command when <leader>aa is pressed

--- dot_config/nvim/lua/plugins/nvim-aibo.lua
+++ dot_config/nvim/lua/plugins/nvim-aibo.lua
@@ -5,12 +5,38 @@ return {
 		{
 			"<leader>aa",
 			function()
-				vim.api.nvim_command("Aibo -opener=botright\\ vsplit -toggle codex")
+				vim.api.nvim_command("Aibo -opener=botright\\ vsplit -toggle codex resume")
 			end,
 			desc = "Aibo Codex",
 		},
 	},
 	config = function()
-		require("aibo").setup()
+		require("aibo").setup({
+			disable_startinsert_on_insert = false,
+			disable_startinsert_on_startup = true,
+			prompt = {
+				on_attach = function(bufnr)
+          vim.o.hidden = true
+					vim.api.nvim_create_autocmd("VimLeavePre", {
+						group = vim.api.nvim_create_augroup("AiboClose", { clear = true }),
+						buffer = bufnr,
+						callback = function()
+							vim.cmd("bwipeout! " .. bufnr)
+						end,
+					})
+				end,
+			},
+      console = {
+        on_attach = function(bufnr)
+          vim.api.nvim_create_autocmd("VimLeavePre", {
+            group = vim.api.nvim_create_augroup("AiboClose", { clear = true }),
+            buffer = bufnr,
+            callback = function()
+              vim.cmd("bwipeout! " .. bufnr)
+            end,
+          })
+        end,
+      }
+		})
 	end,
 }
