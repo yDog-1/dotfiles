9346bf77f473c8eadd3cfccb6e599e9b3718a72d
yDog <ydog-1@outlook.jp>
2025-11-03 13:35:05 +0900

    style(nvim): fix formatting of utils.lua

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -163,7 +163,7 @@ return {
 		event = "InsertEnter",
 		init = function()
 			vim.g.lexima_ctrlh_as_backspace = 1
-      vim.g.lexima_no_default_rules = 1
+			vim.g.lexima_no_default_rules = 1
 		end,
 	},
 	-- 囲い文字を上手く扱えるように
