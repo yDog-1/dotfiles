9e338118151a91cf62cb5292b412904eaae35e72
yDog <ydog-1@outlook.jp>
2025-11-03 10:01:48 +0900

    fix(nvim): emmylua_lspの`workspace.library`の設定を修正
    vim.api.nvim_get_runtime_file(<name>, true)の<name>は、
    ファイル名の正規表現が入るもので、全てのファイルを対象
    にしたいため、空文字に変更した。

--- dot_config/nvim/lua/plugins/lsp/init.lua
+++ dot_config/nvim/lua/plugins/lsp/init.lua
@@ -86,7 +86,7 @@ return {
 				settings = {
 					Lua = {
 						workspace = {
-							library = vim.api.nvim_get_runtime_file("lua", true),
+							library = vim.api.nvim_get_runtime_file("", true),
 						},
 					},
 				},
