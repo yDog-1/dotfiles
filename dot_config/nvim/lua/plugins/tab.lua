dbfda4f57eecca21ed95dcf6b8f8b0a0fa78e065
yDog <ydog-1@outlook.jp>
2026-01-18 15:37:40 +0900

    feat(nvim): change keymaps for tab navigation to start with <c-t>

--- dot_config/nvim/lua/plugins/tab.lua
+++ dot_config/nvim/lua/plugins/tab.lua
@@ -1,13 +1,14 @@
 require("plugins.which-key.spec").add({
 	mode = "n",
-	{ "<c-p>", group = "tab" },
+	{ "<c-t>", group = "tab" },
 })
 
-vim.keymap.set("n", "<c-p>n", "<cmd>tabnew<cr>", { desc = "new tab" })
-vim.keymap.set("n", "<c-p>d", "<cmd>tabclose<cr>", { desc = "close tab" })
-vim.keymap.set("n", "<c-p>o", "<cmd>tabonly<cr>", { desc = "close other tabs" })
-vim.keymap.set("n", "<c-p>p", "<cmd>tabnext<cr>", { desc = "next tab" })
-vim.keymap.set("n", "<c-p><c-p>", "<cmd>tabprevious<cr>", { desc = "previous tab" })
+vim.keymap.set("n", "<c-t>n", "<cmd>tabnew<cr>", { desc = "new tab" })
+vim.keymap.set("n", "<c-t>q", "<cmd>tabclose<cr>", { desc = "close tab" })
+vim.keymap.set("n", "<c-t>o", "<cmd>tabonly<cr>", { desc = "close other tabs" })
+vim.keymap.set("n", "<c-t>l", "<cmd>tabnext<cr>", { desc = "next tab" })
+vim.keymap.set("n", "<c-t><c-t>", "<cmd>tabnext<cr>", { desc = "next tab" })
+vim.keymap.set("n", "<c-t>h", "<cmd>tabprevious<cr>", { desc = "previous tab" })
 
 return {
 	{
