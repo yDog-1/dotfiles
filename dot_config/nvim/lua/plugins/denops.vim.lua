bb788c1cf8d3d6c07022d02f5e970be39a916fce
yDog <ydog-1@outlook.jp>
2026-05-08 21:16:24 +0900

    feat(nvim): add `DenopsFixCache` command to clear cache of denops.vim

--- dot_config/nvim/lua/plugins/denops.vim.lua
+++ dot_config/nvim/lua/plugins/denops.vim.lua
@@ -17,6 +17,11 @@ return {
 		build = function()
 			vim.fn["denops_shared_server#install"]()
 		end,
+		config = function()
+			vim.api.nvim_create_user_command("DenopsFixCache", function()
+				vim.fn["denops#cache#update"]({ reload = true })
+			end, { desc = "Fix Denops cache" })
+		end,
 	},
 	{
 		"yuki-yano/denops-lazy.nvim",
