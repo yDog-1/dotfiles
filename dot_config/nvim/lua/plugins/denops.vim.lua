f28fb149ac6771625c13241e6f81023ba119f270
yDog <ydog-1@outlook.jp>
2026-05-09 20:45:46 +0900

    feat(nvim): remove denops-shared-server.vim
    PCを起動するたびに、サーバーが自動実行されない不具合が発生
    原因不明、調査もしていないため、当該プラグインを削除

--- dot_config/nvim/lua/plugins/denops.vim.lua
+++ dot_config/nvim/lua/plugins/denops.vim.lua
@@ -2,9 +2,6 @@ return {
 	{
 		"vim-denops/denops.vim",
 		lazy = false,
-		dependencies = {
-			"vim-denops/denops-shared-server.vim",
-		},
 		init = function()
 			vim.g["denops#server#deno_args"] = {
 				"-q",
@@ -12,7 +9,6 @@ return {
 				"-A",
 				"--unstable-kv",
 			}
-			vim.g["denops_server_addr"] = "127.0.0.1:32123"
 		end,
 		build = function()
 			vim.fn["denops_shared_server#install"]()
