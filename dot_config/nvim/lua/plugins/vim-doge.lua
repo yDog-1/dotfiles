d69e7135fdb0134149bdd80cdb687511ac4f5af9
yDog <ydog-1@outlook.jp>
2026-05-17 15:50:34 +0900

    fix(vim-doge): only run `doge#install()` on plugin's build step

--- dot_config/nvim/lua/plugins/vim-doge.lua
+++ dot_config/nvim/lua/plugins/vim-doge.lua
@@ -1,11 +1,13 @@
 return {
 	"kkoomen/vim-doge",
 	lazy = false,
+	build = function()
+		vim.cmd("silent call doge#install()")
+	end,
 	init = function()
 		vim.g.doge_enable_mappings = 0
 	end,
 	config = function()
-		vim.cmd("silent call doge#install()")
 		vim.keymap.set("n", "<leader>cm", "<cmd>DogeGenerate<CR>", { desc = "Generate Documentation" })
 	end,
 }
