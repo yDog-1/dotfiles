682605f2be8cf742dd8d14939d8b76c64f9aac46
yDog <ydog-1@outlook.jp>
2026-05-17 14:11:32 +0900

    Revert "feat(nvim): remove denops-shared-server.vim"
    This reverts commit f28fb149ac6771625c13241e6f81023ba119f270.

--- dot_config/nvim/lua/plugins/denops.vim.lua
+++ dot_config/nvim/lua/plugins/denops.vim.lua
@@ -2,6 +2,9 @@ return {
 	{
 		"vim-denops/denops.vim",
 		lazy = false,
+		dependencies = {
+			"vim-denops/denops-shared-server.vim",
+		},
 		init = function()
 			vim.g["denops#server#deno_args"] = {
 				"-q",
@@ -9,6 +12,7 @@ return {
 				"-A",
 				"--unstable-kv",
 			}
+			vim.g["denops_server_addr"] = "127.0.0.1:32123"
 		end,
 		build = function()
 			vim.fn["denops_shared_server#install"]()
