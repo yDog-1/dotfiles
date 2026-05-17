e2531f78c86f8ea678e49f37b18471bf8c9682e0
yDog <ydog-1@outlook.jp>
2026-05-17 13:43:25 +0900

    feat(wezterm): disable tabbar, I don't use tab

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -139,9 +139,7 @@ config.window_background_opacity = 0.80
 config.window_close_confirmation = "NeverPrompt"
 
 -- タブの見た目
-config.use_fancy_tab_bar = false
-config.show_new_tab_button_in_tab_bar = false
-config.tab_max_width = 32
+config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.window_padding = {
 	left = 0,
